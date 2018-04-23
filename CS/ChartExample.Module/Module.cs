5c573d1a7aad599513b440e2d5d2606d1f27c003
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ChartExample.Module/Module.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace ChartExample.Module {
+    public sealed partial class ChartExampleModule : ModuleBase {
+        public ChartExampleModule() {
+            InitializeComponent();
+        }
+    }
+}
